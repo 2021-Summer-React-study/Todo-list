2c4ad0e858a534fc899fa0ffdcf25228dcb914a7
gimume <yum17201619@gmail.com>
2021-08-01 00:17:38 +0900

    add first project(10)

---
+++ src/components/TodoListItem.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {
+    MdCheckBoxOutlineBlank,
+    MdCheckBox,
+    MdRemoveCircleOutline,
+} from 'react-icons/md';
+import cn from 'classnames' // 조건부로 classNames 결합할 수 있는 자스 유틸리티
+import "./TodoListItem.scss";
+const TodoListItem = ({todo, onRemove, onToggle}) => {
+    const {id, text, checked} = todo;
+    return (
+    <div className="TodoListItem">
+        <div className={cn('checkbox', {checked})} onClick={() => onToggle(id)}>
+            {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+            <div className="text">{text}</div>
+        </div>
+        <div className="remove" onClick={() => onRemove(id)}>
+            <MdRemoveCircleOutline />
+        </div>
+    </div>
+    )
+}
+
+export default TodoListItem;
